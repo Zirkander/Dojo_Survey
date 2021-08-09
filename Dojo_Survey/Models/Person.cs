aa28aa58cf399d3f685007c294bba697c47ee44b
Lawrence <45595421+Zirkander@users.noreply.github.com>
2021-08-08 22:04:40 -0700

    Finished project

--- Dojo_Survey/Models/Person.cs
+++ Dojo_Survey/Models/Person.cs
@@ -8,7 +8,7 @@ namespace Dojo_Survey.Models
         public string Name { get; set; }
         public string Location { get; set; }
         public string Language { get; set; }
-        [MaxLength(65)]
+        [MaxLength(20)]
         public string Comment { get; set; }
         [Required]
         [MinLength(5)]
